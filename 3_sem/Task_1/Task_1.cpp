b9562c3efbc47411a7416ec599d6840d93f9cc06
pockettaiga <oleg.chen.07@gmail.com>
2022-09-20 23:37:17 +0300

    +sfml

--- 3_sem/Task_1/Task_1.cpp
+++ 3_sem/Task_1/Task_1.cpp
@@ -1,5 +1,15 @@
-﻿
+﻿#define NOMINMAX
+
+#include <Windows.h>
+#include <stdlib.h>
+#include <math.h>
+#include <SFML/Graphics.hpp>
+
+#include <iostream>
+using namespace std;
+
 
 int main() {
+	cout << "hello world!";
 	return 0;
 }
